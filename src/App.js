64378d240b80473a67bd8e1b80f2a15029c44be2
Kavitha <selvikavitha362@gmail.com>
2025-07-27 13:00:47 +0530

    Fix routing for GitHub Pages using basename

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Layout } from 'antd';
@@ -48,7 +22,7 @@ const AppLayout = ({ children }) => {
 
 function App() {
   return (
-    <Router>
+    <Router basename="/user-list-app">
       <AppLayout>
         <Routes>
           <Route path="/" element={<Login />} />
